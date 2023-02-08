ed9eae8bef64cde4430382072fc285192d650857
Vedant Patil <106603826+vedantpatil25@users.noreply.github.com>
2023-02-08 18:17:31 +0530

    Added solution - LeetHub

--- Binary Tree Representation - GFG/binary-tree-representation.cpp
+++ Binary Tree Representation - GFG/binary-tree-representation.cpp
@@ -56,19 +56,27 @@ public:
     void create_tree(node* root0, vector<int> &vec){
         
         //Your code goes here
-        root0->data=vec[0];
-        node* root1=newNode(vec[1]);
-        node* root2=newNode(vec[2]);
-        node* root3=newNode(vec[3]);
-        node* root4=newNode(vec[4]);
-        node* root5=newNode(vec[5]);
-        node* root6=newNode(vec[6]);
-        root0->left=root1;
-        root0->right=root2;
-        root1->left=root3;
-        root1->right=root4;
-        root2->left=root5;
-        root2->right=root6;
+        queue<node*>q;
+        q.push(root0);
+        
+        int i = 1;
+        int n = vec.size();
+        while(i < n)
+        {
+            node * root = q.front();
+            q.pop();
+            
+            node * left = newNode(vec[i]);
+            root->left = left;
+            i++;
+            q.push(left);
+            
+            
+            node * right = newNode(vec[i]);
+            root->right = right;
+            i++;
+            q.push(right);
+        }
     }
 };
 
