c2dd6cbbee64fb92b0071f6cc2a928f89f777520
Vedant Patil <106603826+vedantpatil25@users.noreply.github.com>
2023-05-02 09:48:17 +0530

    Added solution - LeetHub

---
+++ Cake Distribution Problem - GFG/cake-distribution-problem.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+    public:
+    int maxSweetness(vector<int>& arr, int n, int k) {
+        long long lo = 1, hi = 0, ans = 0;
+        for(int &e: arr) hi += e;
+        if(k == 0) return hi;
+        
+        while(lo <= hi) {
+            long long mid = (lo+hi)/2;
+            long long ok = func(mid, arr, n, k);
+            if(ok) {
+                lo = mid+1;
+                ans = max(ans, ok);
+            }
+            else hi = mid-1;
+        }
+        return ans;
+    }
+    long long func(long long val, vector<int> &arr, int n, int k) {
+        long long mi = LLONG_MAX, sum = 0, cur = 0;
+        for(int i = 0; i < n; i++) {
+            if(sum < val) sum += arr[i];
+            else {
+                mi = min(mi, sum);
+                sum = arr[i], cur++;
+                if(cur == k) {
+                    for(i++; i < n; i++) sum += arr[i];
+                    if(sum >= val) {
+                        mi = min(mi, sum);
+                        sum = arr[i], cur++;
+                    }
+                }
+            }
+        }
+        if(cur < k+1) return 0;
+        return mi;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> sweetness(n);
+        for (int i = 0; i < n; i++) {
+            cin >> sweetness[i];
+        }
+          Solution ob;
+          int res=ob.maxSweetness(sweetness, n, k);
+        cout << res << endl;
+    }
+}
+// } Driver Code Ends
