822b18c9113f1befc6e2e5180f06cffcc8d9d0e3
Vedant Patil <106603826+vedantpatil25@users.noreply.github.com>
2023-10-17 01:55:26 +0530

    Added solution - LeetHub

---
+++ Geek's Training - GFG/geeks-training.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int solve(int day, int last, vector<vector<int>>&points,  vector<vector<int>>&dp){
+        
+        //base case
+        if(day == 0){
+            int maxi = 0 ; 
+            for(int i = 0 ; i < 3; i++){
+                if(i != last) maxi = max(maxi, points[0][i]);
+            }
+            return maxi;
+        }
+        
+        //optimising recurssion using dp
+        if(dp[day][last] != -1) return dp[day][last];
+        
+        //logic
+        int maxi = 0;
+        for(int i = 0 ; i < 3 ; i++){
+            if(i != last){
+                int point = points[day][i] + solve(day-1, i, points, dp);
+                maxi = max(maxi, point);
+            }
+        }
+        return dp[day][last] = maxi;
+    }
+    int maximumPoints(vector<vector<int>>& points, int n) {
+        // Code here
+        vector<vector<int>>dp(n,vector<int>(4 , -1));
+        int ans = solve(n-1, 3 ,points, dp);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> points;
+        for (int i = 0; i < n; ++i) {
+            vector<int> temp;
+            for (int j = 0; j < 3; ++j) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            points.push_back(temp);
+        }
+
+        Solution obj;
+        cout << obj.maximumPoints(points, n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
